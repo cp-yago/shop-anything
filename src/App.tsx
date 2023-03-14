e1074cf5a1e35a146fe92e74bcea164bef4b0b2d
cp-yago <yago.cunha123@gmail.com>
2023-03-13 22:08:21 -0300

    feat: add tanstack query to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Home } from './pages/Home';
 import { Checkout } from './pages/Checkout';
 import { Success } from './pages/Success';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 import { ShoppingContextProvider } from './contexts/ShoppingContext';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -25,9 +28,11 @@ const router = createBrowserRouter([
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ShoppingContextProvider>
-        <RouterProvider router={router} />
-      </ShoppingContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ShoppingContextProvider>
+          <RouterProvider router={router} />
+        </ShoppingContextProvider>
+      </QueryClientProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
